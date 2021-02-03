5ad0d69ff2cce22076843b69f2f38085e0413752
gideaopinheiro <gpss@ic.ufal.br>
2021-02-03 18:36:35 -0300

    chore: initial project setup

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/presentation/errors/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
