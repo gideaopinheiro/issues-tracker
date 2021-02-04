89debf219fe66f2f300f8e517520723a792e0967
gideaopinheiro <gpss@ic.ufal.br>
2021-02-03 21:53:41 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -4,9 +4,12 @@ import { badRequest } from '../helpers/http/http-helper'
 
 export class SignUpController implements Controller {
   async handle (request: SignUpController.Request): Promise<HttpResponse> {
-    const { name } = request
+    const { name, email } = request
     if (!name) {
       return badRequest(new MissingParamError('name'))
+    }
+    if (!email) {
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
