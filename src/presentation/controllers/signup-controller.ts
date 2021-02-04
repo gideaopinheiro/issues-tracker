97dc5b9e9f996fcfe71cedc7a140ce2256b442ca
gideaopinheiro <gpss@ic.ufal.br>
2021-02-04 15:43:38 -0300

    feat: ensure SignUpController returns 400 if password and passwordConfirmation does not match

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,9 +1,12 @@
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
+import { CompareFieldsValidation } from '../../validation/validators/compare-fields-validation'
 
 export class SignUpController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly emailValidation: Validation
+  ) {}
 
   async handle (request: SignUpController.Request): Promise<HttpResponse> {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
@@ -12,9 +15,15 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-    const error = this.validation.validate(request.email)
-    if (error) {
-      return badRequest(error)
+    const emailValidationError = this.emailValidation.validate(request.email)
+    if (emailValidationError) {
+      return badRequest(emailValidationError)
+    }
+
+    const compareFieldsValidation = new CompareFieldsValidation('password', 'passwordConfirmation')
+    const compareFieldsError = compareFieldsValidation.validate({ password: request.password, passwordConfirmation: request.passwordConfirmation })
+    if (compareFieldsError) {
+      return badRequest(compareFieldsError)
     }
   }
 }
