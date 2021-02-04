1d7802dce7ad99e3e6d08b33c48432c1e7394b4c
gideaopinheiro <gpss@ic.ufal.br>
2021-02-04 08:55:50 -0300

    test: ensure SignUpController calls emailValidator with correct value

--- tests/presentation/controllers/signup-controller.spec.ts
+++ tests/presentation/controllers/signup-controller.spec.ts
@@ -1,15 +1,27 @@
 import { Controller } from '@/presentation/protocols/controller'
 import { SignUpController } from '@/presentation/controllers/signup-controller'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { mockEmailValidator } from '../mocks/mock-email-validator'
+import { EmailValidator } from '@/validation/protocols/email-validator'
+import { mockRequest } from '../mocks/mock-request'
 
-const makeSut = (): Controller => {
-  const sut = new SignUpController()
-  return sut
+type SutTypes = {
+  sut: Controller
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = mockEmailValidator()
+  const sut = new SignUpController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('SignUpController', () => {
   test('Should return 400 if no name is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({
       email: 'any_email@mail.com',
       password: 'any_password',
@@ -20,7 +32,7 @@ describe('SignUpController', () => {
   })
 
   test('Should return 400 if no email is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({
       name: 'any_name',
       password: 'any_password',
@@ -28,5 +40,12 @@ describe('SignUpController', () => {
     })
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should call emailValidator with correct value', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    await sut.handle(mockRequest())
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
