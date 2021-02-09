605da51bfd310de6c66392eba7ba6c5997321fbb
gideaopinheiro <gpss@ic.ufal.br>
2021-02-09 10:46:51 -0300

    feat: ensure requests parse body as json

---
+++ tests/main/middlewares/body-parser.test.ts
@@ -0,0 +1,13 @@
+import app from '@/main/config/app'
+import request from 'supertest'
+
+describe('BodyParser Middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app).post('/test_body_parser')
+      .send({ name: 'Gideao' })
+      .expect({ name: 'Gideao' })
+  })
+})
