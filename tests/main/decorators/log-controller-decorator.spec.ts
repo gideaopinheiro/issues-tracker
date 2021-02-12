374f85a548512f78711ca6f518bd2d0053ecbe26
gideaopinheiro <gpss@ic.ufal.br>
2021-02-12 17:44:46 -0300

    test: ensure LogControllerDecorator returns 500 if Controller returns 500

---
+++ tests/main/decorators/log-controller-decorator.spec.ts
@@ -0,0 +1,44 @@
+import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
+import { ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { Controller, HttpResponse } from '@/presentation/protocols'
+import { mockAccount } from '@/tests/domain/mocks'
+import { mockRequest } from '@/tests/presentation/mocks'
+
+const mockController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (request: any): Promise<HttpResponse> {
+      return ok(mockAccount())
+    }
+  }
+  return new ControllerStub()
+}
+
+type SutTypes = {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = mockController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogController Decorator', () => {
+  test('should call Controller with correct values', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    await sut.handle(mockRequest())
+    expect(handleSpy).toHaveBeenCalledWith(mockRequest())
+  })
+
+  test('should return 500 if Controller returns 500', async () => {
+    const { sut, controllerStub } = makeSut()
+    jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(Promise.resolve(serverError()))
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError())
+  })
+})
