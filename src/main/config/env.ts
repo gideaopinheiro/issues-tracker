66ab44cd33c6d11205ca0242d4f0ad693c2f1d9d
gideaopinheiro <gpss@ic.ufal.br>
2021-02-16 08:42:16 -0300

    refactor: adjuste mongodb url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-ts-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/issues-tracker-api',
   port: process.env.PORT || 5050
 }
