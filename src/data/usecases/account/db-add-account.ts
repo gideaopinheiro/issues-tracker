0d5e92e390c5479c008214aae08c92e769507c8a
gideaopinheiro <gpss@ic.ufal.br>
2021-02-17 11:10:01 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct value

--- src/data/usecases/account/db-add-account.ts
+++ src/data/usecases/account/db-add-account.ts
@@ -1,18 +1,24 @@
-import { AccountModel } from '@/domain/models/account'
-import { AddAccount } from '@/domain/usecases'
 import { Hasher } from '@/data/protocols/criptography/hasher'
 import { AddAccountRepository } from '@/data/protocols/db/add-account-repository'
+import { AccountModel } from '@/domain/models/account'
+import { AddAccount } from '@/domain/usecases'
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/load-account-by-email-repository'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
   async add (accountParams: AddAccount.Params): Promise<AccountModel> {
-    const { password } = accountParams
-    const hashedPassword = await this.hasher.hash(password)
-    const account = await this.addAccountRepository.addAccount(Object.assign({}, accountParams, { password: hashedPassword }))
-    return account
+    const { email, password } = accountParams
+    const account = await this.loadAccountByEmailRepository.loadByEmail(email)
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(password)
+      const newAccount = await this.addAccountRepository.addAccount(Object.assign({}, accountParams, { password: hashedPassword }))
+      return newAccount
+    }
+    return null
   }
 }
