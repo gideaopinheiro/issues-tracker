274742295eb57a6f82540ed55be67d1e6a357bf2
gideaopinheiro <gpss@ic.ufal.br>
2021-02-17 10:57:10 -0300

    refactor: change file name

--- src/infra/db/mongodb/account-repository/add-account-mongo-repository.ts
+++ src/infra/db/mongodb/account-repository/add-account-mongo-repository.ts
@@ -3,7 +3,7 @@ import { AccountModel } from '@/domain/models/account'
 import { AddAccount } from '@/domain/usecases'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AddAccountMongoRepository implements AddAccountRepository {
   async addAccount (accountParams: AddAccount.Params): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountParams)
