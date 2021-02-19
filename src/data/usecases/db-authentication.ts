0ca7b2fb397fcc47a87762bd3762033d48ed4b74
gideaopinheiro <gpss@ic.ufal.br>
2021-02-19 12:42:55 -0300

    feat: ensure DbAuthentication returns null if loadAccountByEmailRepository returns null

--- src/data/usecases/db-authentication.ts
+++ src/data/usecases/db-authentication.ts
@@ -5,7 +5,9 @@ export class DbAuthentication implements Authentication {
   constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) { }
 
   async auth (credentials: AuthenticationParams): Promise<string> {
-    await this.loadAccountByEmailRepository.loadByEmail(credentials.email)
-    return null
+    const account = await this.loadAccountByEmailRepository.loadByEmail(credentials.email)
+    if (!account) {
+      return null
+    }
   }
 }
