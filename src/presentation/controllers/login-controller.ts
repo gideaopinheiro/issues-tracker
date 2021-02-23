5554840b85e68f315c300711c7beb1059c5d51b4
gideaopinheiro <gpss@ic.ufal.br>
2021-02-22 22:44:52 -0300

    feat: ensure LoginController calls authentication with correct values

--- src/presentation/controllers/login-controller.ts
+++ src/presentation/controllers/login-controller.ts
@@ -1,14 +1,19 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
-import { badRequest } from '../helpers/http/http-helper'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
+import { Authentication } from '@/domain/usecases/authentication'
 
 export class LoginController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
+  ) {}
 
   async handle (request: LoginController.Request): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+    await this.authentication.auth(request)
     return null
   }
 }
