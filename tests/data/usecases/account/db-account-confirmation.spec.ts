694764a738cdb187c803ef8dfec866c2162e7201
gideaopinheiro <gpss@ic.ufal.br>
2021-03-10 20:07:56 -0300

    test: ensure DbAccountConfirmation calls updateAccountRepository with correct values

--- tests/data/usecases/account/db-account-confirmation.spec.ts
+++ tests/data/usecases/account/db-account-confirmation.spec.ts
@@ -2,6 +2,7 @@ import { LoadAccountByConfirmationTokenRepository, UpdateAccountRepository } fro
 import { DbAccountConfirmation } from '@/data/usecases/account/db-account-confirmation'
 import { AccountConfirmation } from '@/domain/usecases'
 import { mockLoadAccountByConfirmationTokenRepository, mockUpdateAccountRepository } from '@/tests/data/mocks'
+import { mockAccount } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: AccountConfirmation
@@ -26,5 +27,12 @@ describe('DbAccountConfirmation usecase', () => {
     const loadByTokenSpy = jest.spyOn(loadAccountByConfirmationTokenRepositoryStub, 'loadByToken')
     await sut.confirm('any_confirmation_token')
     expect(loadByTokenSpy).toHaveBeenCalledWith('any_confirmation_token')
+  })
+
+  test('should call updateAccountRepository with correct value', async () => {
+    const { sut, updateAccountRepositoryStub } = makeSut()
+    const updateAccountSpy = jest.spyOn(updateAccountRepositoryStub, 'updateAccount')
+    await sut.confirm('any_confirmation_token')
+    expect(updateAccountSpy).toHaveBeenCalledWith(mockAccount())
   })
 })
