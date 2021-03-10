84f1e2c1ff2f724d05ceae54191a7927bfd00ba1
gideaopinheiro <gpss@ic.ufal.br>
2021-03-10 16:34:35 -0300

    feat: add confirmation router

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,12 @@
 import { Express, Router } from 'express'
 import signUpRouter from '@/main/routes/signup-routes'
 import loginRouter from '@/main/routes/login-routes'
+import confirmationRouter from '@/main/routes/confirmation-routes'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   signUpRouter(router)
   loginRouter(router)
+  confirmationRouter(router)
 }
