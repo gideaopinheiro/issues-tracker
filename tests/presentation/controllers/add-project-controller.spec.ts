b2c6acb599e7646d6b21c6a32f060e2727fca4f5
gideaopinheiro <gpss@ic.ufal.br>
2021-03-13 15:48:55 -0300

    test: ensure AddProjectController returns 400 if validation fails

--- tests/presentation/controllers/add-project-controller.spec.ts
+++ tests/presentation/controllers/add-project-controller.spec.ts
@@ -1,4 +1,6 @@
 import { AddProjectController } from '@/presentation/controllers/add-project-controller'
+import { MissingParamError } from '@/presentation/errors'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Controller, Validation } from '@/presentation/protocols'
 import { mockProjectParams } from '@/tests/domain/mocks'
 import { mockProjectValidation } from '@/tests/presentation/mocks'
@@ -27,5 +29,12 @@ describe('CreateProjectController', () => {
       title: 'any_title',
       description: 'any_description'
     })
+  })
+
+  it('should return 400 if validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('title'))
+    const error = await sut.handle(mockProjectParams())
+    expect(error).toEqual(badRequest(new MissingParamError('title')))
   })
 })
