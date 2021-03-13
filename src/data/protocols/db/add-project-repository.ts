8c9a5cf2740c6223f5e0c58add3386816afc8936
gideaopinheiro <gpss@ic.ufal.br>
2021-03-13 12:48:47 -0300

    refactor: change addProjectRepository types

--- src/data/protocols/db/add-project-repository.ts
+++ src/data/protocols/db/add-project-repository.ts
@@ -2,5 +2,10 @@ import { ProjectModel } from '@/domain/models'
 import { AddProject } from '@/domain/usecases'
 
 export interface AddProjectRepository {
-  addProject: (params: AddProject.Params) => Promise<ProjectModel>
+  addProject: (params: AddProjectRepository.Params) => Promise<AddProjectRepository.Result>
+}
+
+export namespace AddProjectRepository {
+  export type Params = AddProject.Params
+  export type Result = ProjectModel
 }
