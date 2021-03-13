bb2f575d29c42527d7df54ccd6fdbefdbd5491e5
gideaopinheiro <gpss@ic.ufal.br>
2021-03-13 15:44:58 -0300

    feat: ensure AddProjectController returns 400 if validation fails

--- src/presentation/controllers/add-project-controller.ts
+++ src/presentation/controllers/add-project-controller.ts
@@ -1,11 +1,15 @@
 import { ProjectModel } from '@/domain/models'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class AddProjectController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (params: CreateProjectController.Params): Promise<HttpResponse> {
-    this.validation.validate(params)
+    const error = this.validation.validate(params)
+    if (error) {
+      return badRequest(error)
+    }
     return null
   }
 }
