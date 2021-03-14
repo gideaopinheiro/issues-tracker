5ae85197cf299c16561b8cd2f03a500acbcf8da8
gideaopinheiro <gpss@ic.ufal.br>
2021-03-14 20:04:39 -0300

    test: ensure AddProjectController calls AddProject usecase with correct values

--- tests/presentation/controllers/add-project-controller.spec.ts
+++ tests/presentation/controllers/add-project-controller.spec.ts
@@ -2,20 +2,24 @@ import { AddProjectController } from '@/presentation/controllers/add-project-con
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Controller, Validation } from '@/presentation/protocols'
-import { mockProjectParams } from '@/tests/domain/mocks'
+import { mockAddProject, mockProjectParams } from '@/tests/domain/mocks'
 import { mockProjectValidation } from '@/tests/presentation/mocks'
+import { AddProject } from '@/domain/usecases'
 
 type SutTypes = {
   sut: Controller
   validationStub: Validation
+  addProjectStub: AddProject
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = mockProjectValidation()
-  const sut = new AddProjectController(validationStub)
+  const addProjectStub = mockAddProject()
+  const sut = new AddProjectController(validationStub, addProjectStub)
   return {
     sut,
-    validationStub
+    validationStub,
+    addProjectStub
   }
 }
 
@@ -36,5 +40,12 @@ describe('CreateProjectController', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('title'))
     const error = await sut.handle(mockProjectParams())
     expect(error).toEqual(badRequest(new MissingParamError('title')))
+  })
+
+  it('should call addProject with correct values', async () => {
+    const { sut, addProjectStub } = makeSut()
+    const addSpy = jest.spyOn(addProjectStub, 'add')
+    await sut.handle(mockProjectParams())
+    expect(addSpy).toHaveBeenCalledWith(mockProjectParams())
   })
 })
