49d80e10a8998412c97c021cd9d01b0c75f94b78
gideaopinheiro <gpss@ic.ufal.br>
2021-03-30 10:22:45 -0300

    test: ensure DbAddTicketComment calls AddTicketCommentRepository with correct value

---
+++ tests/data/usecases/project/db-add-ticket-comment.spec.ts
@@ -0,0 +1,27 @@
+import { AddTicketCommentRepository } from '@/data/protocols/db'
+import { DbAddTicketComment } from '@/data/usecases/project/db-add-ticket-comment'
+import { mockCommentParams } from '@/tests/domain/mocks'
+import { mockAddTicketCommentRepository } from '@/tests/data/mocks'
+
+type SutTypes = {
+  sut: DbAddTicketComment
+  addTicketCommentRepositoryStub: AddTicketCommentRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addTicketCommentRepositoryStub = mockAddTicketCommentRepository()
+  const sut = new DbAddTicketComment(addTicketCommentRepositoryStub)
+  return {
+    sut,
+    addTicketCommentRepositoryStub
+  }
+}
+
+describe('DbAddTicketComment', () => {
+  test('should call AddTicketCommentRepository with correct value', async () => {
+    const { sut, addTicketCommentRepositoryStub } = makeSut()
+    const addCommentSpy = jest.spyOn(addTicketCommentRepositoryStub, 'addComment')
+    await sut.add(mockCommentParams())
+    expect(addCommentSpy).toHaveBeenCalledWith(mockCommentParams())
+  })
+})
