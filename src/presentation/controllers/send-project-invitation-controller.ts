b7fd6f858615503aa62171109a028e503a8175f4
gideaopinheiro <gpss@ic.ufal.br>
2021-04-06 10:34:22 -0300

    feat: ensure SendProjectInvitationController calls SendProjectInvitationRepository w/ correct values

--- src/presentation/controllers/send-project-invitation-controller.ts
+++ src/presentation/controllers/send-project-invitation-controller.ts
@@ -1,15 +1,20 @@
 import { ProjectInvitationModel } from '@/domain/models'
 import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
+import { SendProjectInvitationRepository } from '@/data/protocols/db/send-project-invitation-repository'
 
 export class SendProjectInvitationController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly sendProjectInvitationRepository: SendProjectInvitationRepository
+  ) {}
 
   async handle (params: SendProjectInvitationController.Params): Promise<HttpResponse> {
     const error = this.validation.validate(params)
     if (error) {
       return badRequest(error)
     }
+    await this.sendProjectInvitationRepository.sendProjectInvitation(params)
     return null
   }
 }
