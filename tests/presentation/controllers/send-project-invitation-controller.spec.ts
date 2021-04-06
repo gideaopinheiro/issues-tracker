74597dbb629c4be02ee65075875e1b790eb5aa2d
gideaopinheiro <gpss@ic.ufal.br>
2021-04-06 10:34:40 -0300

    test: ensure SendProjectInvitationController calls SendProjectInvitationRepository w/ correct values

--- tests/presentation/controllers/send-project-invitation-controller.spec.ts
+++ tests/presentation/controllers/send-project-invitation-controller.spec.ts
@@ -1,21 +1,26 @@
+import { SendProjectInvitationRepository } from '@/data/protocols/db/send-project-invitation-repository'
 import { SendProjectInvitationController } from '@/presentation/controllers/send-project-invitation-controller'
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Controller, Validation } from '@/presentation/protocols'
+import { mockSendProjectInvitationRepository } from '@/tests/data/mocks/mock-send-project-invitation-repository'
 import { mockProjectInvitation } from '@/tests/domain/mocks/mock-project-invitation'
 import { mockValidation } from '@/tests/presentation/mocks'
 
 type SutTypes = {
   sut: Controller
   validationStub: Validation
+  sendProjectInvitationRepositoryStub: SendProjectInvitationRepository
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = mockValidation()
-  const sut = new SendProjectInvitationController(validationStub)
+  const sendProjectInvitationRepositoryStub = mockSendProjectInvitationRepository()
+  const sut = new SendProjectInvitationController(validationStub, sendProjectInvitationRepositoryStub)
   return {
     sut,
-    validationStub
+    validationStub,
+    sendProjectInvitationRepositoryStub
   }
 }
 
@@ -32,5 +37,12 @@ describe('SendProjectInvitation Controller', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('from'))
     const httpResponse = await sut.handle(mockProjectInvitation())
     expect(httpResponse).toEqual(badRequest(new MissingParamError('from')))
+  })
+
+  test('should call SendProjectInvitationRepository with correct values', async () => {
+    const { sut, sendProjectInvitationRepositoryStub } = makeSut()
+    const sendProjectInvitationSpy = jest.spyOn(sendProjectInvitationRepositoryStub, 'sendProjectInvitation')
+    await sut.handle(mockProjectInvitation())
+    expect(sendProjectInvitationSpy).toHaveBeenCalledWith(mockProjectInvitation())
   })
 })
