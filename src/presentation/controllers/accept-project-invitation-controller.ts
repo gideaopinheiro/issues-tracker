4929fd5478c28dd8b0b4e5add9ed63dd56a1efa5
gideaopinheiro <gpss@ic.ufal.br>
2021-04-30 11:17:16 -0300

    feat: ensure AcceptProjectInvitationController returns 400 if validation fails

--- src/presentation/controllers/accept-project-invitation-controller.ts
+++ src/presentation/controllers/accept-project-invitation-controller.ts
@@ -1,10 +1,14 @@
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class AcceptProjectInvitationController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (params: AcceptProjectInvitationController.Params): Promise<HttpResponse> {
-    this.validation.validate(params)
+    const error = this.validation.validate(params)
+    if (error) {
+      return badRequest(error)
+    }
     return null
   }
 }
